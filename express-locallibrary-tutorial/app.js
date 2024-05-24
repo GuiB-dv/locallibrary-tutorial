6e8f02fcc56b4526d215349100040af6edfcf8d9
GuiB-dv <152321046+GuiB-dv@users.noreply.github.com>
2024-05-24 12:08:38 -0300

    deploying to production

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -7,13 +7,30 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 var app = express();
+
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://admingui:SAZd9NnZ160ksM8j@cluster0.h7dn8fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const dev_db_url =
+"mongodb+srv://admingui:SAZd9NnZ160ksM8j@cluster0.h7dn8fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -25,11 +42,20 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression()); //compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(limiter);
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
